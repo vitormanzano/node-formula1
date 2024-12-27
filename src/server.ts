9a319734ca66a34429c53bd8ea89f40dd3ada6ef
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2024-12-27 16:19:50 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,13 +12,13 @@ const teams = [
     {id: 1, name: "McLaren", base: "Woking, United Kingdom"}, 
     {id: 2, name: "Mercedes", base: "Brackley, United Kingdom"},
     {id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom"}
-]
+];
 
 const drivers = [
     {id: 1, name: "Max Verstappen", team: "Red Bull Racing"},
     {id: 2, name: "Lewis Hamilton", team: "Ferrari"},
     {id: 3, name: "Lando Norris", team: "McLaren"}
-]
+];
 
 server.get("/teams", async(request, response) => {
     response.type("application/json").code(200);
@@ -48,9 +48,9 @@ server.get<{Params: DriverParams}>("/drivers/:id", async (request, response) => 
         response.type("application/json").code(200);
         return {driver};
     }
-})
+});
 
 
 server.listen({port: 3333}, () => {
     console.log("Server init");
-})
+});
